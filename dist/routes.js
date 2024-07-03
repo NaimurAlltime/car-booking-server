e0b2ae9a208f64242822f621aeb8ef9c4684217a
Naimur Rahman <naimur2231@gmail.com>
2024-07-04 00:13:09 +0600

    project updated

--- dist/routes.js
+++ dist/routes.js
@@ -8,6 +8,7 @@ const auth_routes_1 = __importDefault(require("./modules/auth/auth.routes"));
 const cars_routes_1 = __importDefault(require("./modules/cars/cars.routes"));
 const location_routes_1 = __importDefault(require("./modules/location/location.routes"));
 const search_routes_1 = __importDefault(require("./modules/search/search.routes"));
+const payment_routes_1 = __importDefault(require("./modules/payment/payment.routes"));
 const router = express_1.default.Router();
 const routes = [
     {
@@ -25,6 +26,10 @@ const routes = [
     {
         path: "/search",
         route: search_routes_1.default,
+    },
+    {
+        path: "/payments",
+        route: payment_routes_1.default,
     },
 ];
 routes.forEach((route) => router.use(route.path, route.route));
